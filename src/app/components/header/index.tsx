4bfe9b7fc4fb61ba33089542404db1946d14fa31
marevandro <marevandro95@gmail.com>
2025-07-15 16:26:25 -0300

    initial commit

---
+++ src/app/components/header/index.tsx
@@ -0,0 +1,19 @@
+export function Header() {
+  return (
+    <header className="flex px-2 py-4 bg-zinc-900 text-white">
+      <div className="flex items-center justify-between w-full mx-auto max-w-7xl">
+        <div>
+          NextJS
+        </div>
+
+        <nav>
+          <ul className="flex items-center justify-center gap-2">
+            <li>Home</li>
+            <li>Posts</li>
+          </ul>
+        </nav>
+      </div>
+
+    </header>
+  )
+}
