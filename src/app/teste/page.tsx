14abb14bea9e6d20c678bb9dff1d405eed094284
marevandro <marevandro95@gmail.com>
2025-07-22 10:56:02 -0300

    feat: adicionando pagina use client

--- src/app/teste/page.tsx
+++ src/app/teste/page.tsx
@@ -1,22 +1,27 @@
+'use client'
+
+import { useEffect, useState } from "react"
+
 interface PostProps {
   id: number;
   title: string;
   body: string;
 }
 
-interface ResponseProps {
-  posts: PostProps[]
-}
+export default function PostsPage() {
+  const [posts, setPosts] = useState<PostProps[]>([]); 
 
-export default async function PostsPage() {
-  const response = await fetch('https://dummyjson.com/posts');
-  const data: ResponseProps = await response.json();
+  useEffect(() => {
+    fetch('https://dummyjson.com/posts')
+      .then(res => res.json())
+      .then(data => setPosts(data.posts)) 
+  }, [])
 
   return (
     <div>
       <h1 className="text-center mt-5 mb-2 font-bold text-3xl">Todos os posts</h1>
       <div className="flex flex-col gap-4 mx-2">
-        {data.posts.map(post => (
+        {posts.map(post => (
           <div key={post.id} className="bg-gray-200 p-4 rounded-md">
             <h2 className="font-bold">{post?.title}</h2>
             <p>{post.body}</p>
